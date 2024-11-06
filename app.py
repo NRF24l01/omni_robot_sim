9d82a29b42d0fc114ccb4034ef16035f332e5228
stenli <s.nevrovsky@ya.ru>
2024-11-06 19:26:03 +0300

    update field, add auto resize, and right things example

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 import customtkinter as ctk
+
 from PIL import Image, ImageTk
 import math
+
 from modules.omnirobot import OmniRobot
 from modules.field_items import Background
 from modules.pointer import Pointer
 
 
-class App:
-    def __init__(self, root, background="static/images/field.png", xsize=960, ysize=640):
-        self.root = root
-        self.root.geometry(f"{str(xsize)}x{str(ysize)}")
-        self.root.title("Omni Robot Simulation")
-        self.root.resizable(False, False)
+class App(ctk.CTk):
+    def __init__(self, background="static/images/field.png", xsize=960, ysize=640):
+        super().__init__()
+        #self.geometry(f"{str(xsize)}x{str(ysize)}")
+        self.title("Omni Robot Simulation")
 
-        self.canvas = ctk.CTkCanvas(self.root, width=xsize, height=ysize, bg="white")
-        self.canvas.pack()
+        self.resizable(False, False)
+
+        self.canvas = ctk.CTkCanvas(self, width=xsize, height=ysize, bg="white")
+        self.canvas.grid(row=0, column=0)
+
+        self.test = ctk.CTkButton(master=self, text="Pokaifu")
+        self.test.grid(row=0, column=1)
 
         self.xsize = xsize
         self.ysize = ysize
@@ -23,7 +29,7 @@ class App:
 
         self.pointer = Pointer()
 
-        self.root.bind('<Motion>', self.on_move)
+        self.bind('<Motion>', self.on_move)
 
         self.update()
 
@@ -40,14 +46,12 @@ class App:
         robot = OmniRobot(distance_to_wheels=30, wheel_width=30, wheel_height=15)
         robot.draw(self.canvas, x=100, y=200, angle=90)
 
-        self.root.after(1, self.update)
+        self.after(1, self.update)
 
     def on_move(self, event):
         self.pointer.update(event.x, event.y)
 
 
 if __name__ == "__main__":
-    root = ctk.CTk()
-    app = App(root)
-
-    root.mainloop()
+    app = App()
+    app.mainloop()
