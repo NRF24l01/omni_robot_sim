d8c16b9f38e49309691eae613bc0945b832354bf
nrf24l01 <s.nevrovsky@ya.ru>
2024-11-20 12:26:52 +0300

    fixes + file opening

--- app.py
+++ app.py
@@ -61,7 +61,9 @@ class App(ctk.CTk):
         self.save_button = ctk.CTkButton(
             self.opn_sv_frame, text="Сохранить путь", command=self.save_path
         )
-        self.open_button = ctk.CTkButton(self.opn_sv_frame, text="Открыть путь")
+        self.open_button = ctk.CTkButton(
+            self.opn_sv_frame, text="Открыть путь", command=self.open_path
+        )
         self.opn_sv_frame.grid(row=3, column=0, pady=(10, 0), padx=10, sticky="n")
         self.save_button.grid(row=0, column=0, pady=2, padx=2)
         self.open_button.grid(row=0, column=1, pady=2, padx=2)
@@ -208,28 +210,59 @@ class App(ctk.CTk):
             self.reset()
 
     def save_path(self):
-        self.logger.info("User asked to save path")
+        self.logger.info("Asked path saving")
         if len(self.path.path) == 0:
             self.logger.warning("No path, canseling")
             return
         file = filedialog.asksaveasfile(
-            title="Save As",
+            title="Save path As",
             defaultextension=".path",  # Default file extension
             filetypes=[("path file", ("*.path", ".pth")), ("All Files", "*.*")],
         )
         if file:
             self.logger.info("Started saving to", file.name)
+            t = time()
             file_content = {
                 "format": "json-1",
                 "create_time": time(),
                 "start_point": self.path.start_point,
-                "path": self.path.path
+                "path": self.path.path,
             }
             file.write(dumps(file_content))
             file.close()
-            self.logger.info("Saved path to", file.name)
+            self.logger.info("Saved path to", file.name+",", "by", time() - t)
         else:
             self.logger.warning("No path selected")
+
+    def open_path(self):
+        self.logger.info("Asked path opening")
+        if len(self.path.path) != 0:
+            confirm_window = ConfirmationWindow(
+                self,
+                title="Потверждение",
+                message="Вы действительно хотите заменить текущий путь?",
+                buttons=("Заменить", "Галя, у нас отмена"),
+            )
+            self.wait_window(confirm_window)
+            if not confirm_window.result:
+                self.logger.info("User canseled file opening")
+                return
+        file = filedialog.askopenfile(
+            title="Open path",
+            defaultextension=".path",  # Default file extension
+            filetypes=[("path file", ("*.path", ".pth")), ("All Files", "*.*")],
+        )
+        if file:
+            self.logger.info("Start opening file", file.name)
+            t = time()
+            
+            content = loads(file.read())
+            self.path.start_point = content["start_point"]
+            self.path.path = content["path"]
+    
+            self.logger.info("File opened by", round(time() - t, 4))
+        else:
+            self.logger.warning("No file selected")
 
 
 if __name__ == "__main__":
