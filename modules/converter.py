ef1d4b38e246b0608cf2c6e918227b48fd3a82ee
nrf24l01 <s.nevrovsky@ya.ru>
2024-12-07 21:53:34 +0300

    Fixed BIG convertation prolem

--- modules/converter.py
+++ modules/converter.py
@@ -3,26 +3,26 @@ class Converter:
         self.pxx, self.pxy = pxs
         self.mmx, self.mmy = mms
         
-        self.coefx = self.pxx / self.mmx * 10
-        self.coefy = self.pxy / self.mmy * 10
+        self.coefx = self.pxx / self.mmx
+        self.coefy = self.pxy / self.mmy
     
     def pxs_to_mms(self, pxs: tuple[int]|list[int]) -> list[int]:
         px, py = pxs
-        mx = px * self.coefx
-        my = py * self.coefx
+        mx = px / self.coefx
+        my = py / self.coefx
         return [mx, my]
     
     def mms_to_pxs(self, mms: tuple[int]|list[int]) -> list[int]:
         mx, my = mms
-        px = mx / self.coefx
-        py = my / self.coefy
+        px = mx * self.coefx
+        py = my * self.coefy
         return [px, py]
 
     def list_pxs_to_mms(self, pxsl: list[tuple[int]|list[int]]) -> list[list[int]]:
-        ml = [] # ml - not maskine learning, it is mm list
+        mml = []
         for pxs in pxsl:
-            ml.append(self.pxs_to_mms(pxs))
-        return ml
+            mml.append(self.pxs_to_mms(pxs))
+        return mml
     
     def list_mms_to_pxs(self, mmsl: list[tuple[int]|list[int]]) -> list[list[int]]:
         px = []
@@ -32,5 +32,5 @@ class Converter:
 
 
 if __name__ == "__main__":
-    c = Converter((100, 2), (2, 4))
-    print(c.mms_to_pxs(c.pxs_to_mms((100, 100))))
+    c = Converter((960, 640), (3000, 2000))
+    print(c.pxs_to_mms((960, 640)))
